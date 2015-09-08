22e5372c69dc7eec5940d260e35865680b011755
wangpeng <yituwangpeng@gmail.com>
2015-09-08 16:03:27 +0800

    Initial Commit

---
+++ YTHorizontalCollectionLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YTHorizontalCollectionLayout
+//
+//  Created by 王鹏 on 15/9/8.
+//  Copyright (c) 2015年 王鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YTHorizontalCollectionLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YTHorizontalCollectionLayout
+//
+//  Created by 王鹏 on 15/9/8.
+//  Copyright (c) 2015年 王鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
